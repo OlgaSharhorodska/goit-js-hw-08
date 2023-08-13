87fb551699ba28ece5fc92fb1ba0983222a5d4b7
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2023-08-13 23:08:57 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector("iframe")
+const LS_KEY = "videoplayer-current-time";
+
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(updateTime, 1000));
+
+function updateTime({ seconds }) {
+    localStorage.setItem(LS_KEY,seconds);
+}
+
+player.setCurrentTime(localStorage.getItem(LS_KEY)||0)
+
 
